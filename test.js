fa380de89345e93b7784f875147225e84da1c6e0
Angela Lee <angelalee.one@gmail.com>
2018-06-07 09:14:50 -0400

    clean up comments

--- test.js
+++ test.js
@@ -7,58 +7,41 @@ let businessNetworkConnection = new BusinessNetworkConnection();
 function promiseConnect() {
     let businessNetworkDefinition;
     businessNetworkConnection.connect(cardName)
-    .then((result) => {
-    businessNetworkDefinition = result;
-    console.log(result)
-    });
+        .then((result) => {
+            businessNetworkDefinition = result;
+            console.log(result)
+        });
 }
 
 async function asyncConnect() {
     try {
-    let businessNetworkDefinition = await businessNetworkConnection.connect(cardName);
-    console.log(businessNetworkDefinition)
+        let businessNetworkDefinition = await businessNetworkConnection.connect(cardName);
+        console.log(businessNetworkDefinition)
     } catch (error) {
         console.log(error);
     }
 }
 
+async function addUser() {
+    try {
+        let businessNetworkDefinition = await businessNetworkConnection.connect(cardName);
 
+        let factory = businessNetworkDefinition.getFactory();
+        customer = factory.newResource('org.nobul.biznet.participant', 'Customer', 'PID:1234286981');
+        console.log("#############");
+        let participantRegistry = await businessNetworkConnection.getParticipantRegistry('org.nobul.biznet.participant.Customer');
 
-// let factory = businessNetworkDefinition.getFactory();
-// owner = factory.newResource('net.biz.digitalPropertyNetwork', 'Person', 'PID:1234567890');
-// owner.firstName = 'Fred';
-// owner.lastName = 'Bloggs';
-// //add the person
-// //'participant' the getParticipantRegistry API is used
-// let personRegistry = await this.bizNetworkConnection.getParticipantRegistry('net.biz.digitalPropertyNetwork.Person');
-// await personRegistry.add(owner);
+        customer.fName = 'Angela';
+        customer.lName = 'Lee';
+        customer.email = 'test@nobul.com'
 
-    async function addUser() {
-        try {
-            let businessNetworkDefinition = await businessNetworkConnection.connect(cardName);
-
-            let factory = businessNetworkDefinition.getFactory();
-            customer = factory.newResource('org.nobul.biznet.participant', 'Customer', 'PID:1234206981');
-            console.log("#############");
-            let participantRegistry = await businessNetworkConnection.getParticipantRegistry('org.nobul.biznet.participant.Customer');
-         
-            // // Create a new relationship to the vehicle.
-            // var contact = factory.newConcept('Angela', 'Lee', 'test@nobul.com');
-            // // Add the record to the persons array of records.
-            // customer.Contact.push(contact);
-            customer.fName = 'Angela';
-            customer.lName = 'Lee';
-            customer.email = 'test@nobul.com'
-         
-            participantRegistry.add(customer);
-           await businessNetworkConnection.disconnect();
-        } catch(error) {
-            console.log(error);
-            process.exit(1);
-        }
+        participantRegistry.add(customer);
+        await businessNetworkConnection.disconnect();
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
     }
+}
 
 
-  
 
-    addUser();
